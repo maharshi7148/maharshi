dec8cde11a4bd9ded663d7c5b241ffb99a70f92a
maharshi7148 <maharshistudy7148@gmail.com>
2024-03-09 18:42:05 +0530

    C++

---
+++ maths.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+	int main ()
+	
+	{
+		
+		
+		cout<< sqrt(25);
+		cout<<"\n"<<sqrt(89);
+		cout<<"\n"<<round(1.5);
+		cout<<log(4864);
+		cout<<"\n"<<pow(2,6);
+		cout<<abs(89);
+		
+		
+	}
---
+++ string.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	
+int v=8;
+int u=9;
+	cout<<v+u;
+	string a="maharshi";
+	string b="thakkar";
+	cout<<a;
+	cout<<b;
+	cout<<"\n"<<a+b;
+	cout<<"\n";
+	string c="c language";
+	string d="c++ language";
+	c=c.append(d);
+
+	cout<<c;
+	cout<<"\n";
+	cout << c.size();
+	cout<<"\n";
+	cout<<a.size();
+	cout<<b.length();
+	
+	
+	
+	
+}
